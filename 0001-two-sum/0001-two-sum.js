f7308eb73a964dfd30ca646fd4eb23e1a9a2ed82
nitincodes-mobile <nitinverma1177@gmail.com>
2026-07-25 21:52:03 +0530

    Brute Force for an unsorted and sorted Array

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -16,3 +16,37 @@ var twoSum = function(nums, target) {
          numMap.set(nums[i], i);
     }
 };
+
+// Brute Force for unsorted Array 
+
+var twoSum = function (nums, target){
+    for(let i = 0; i < nums.length ; i++){
+        for(let j = i + 1 ; j < nums.length ; j++){
+            if(nums[i] + nums[j] === target){
+                return [i,j]
+            }
+        }
+    
+    }
+}
+
+
+// Brute for Sorted Array 
+var twoSum = function(nums, target){
+    let left = 0;
+    let right = nums.length -1;
+
+    while(left < right){
+        if(nums[left] +  nums[right] > target){
+            right--;
+        } 
+        else if(nums[left] +  nums[right] < target){
+        left++;
+        }
+        else{
+            return [left, right]
+        }
+    }
+
+}
+
